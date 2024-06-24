2d0f33208784f050689d644826d893f3c2efcb84
AkshayaFoiger <akshaya.shankar@juspay.in>
2024-06-24 17:25:15 +0530

    (feat): retrieve payments via transaction process

--- src/modules/webhooks/transaction-process-session.ts
+++ src/modules/webhooks/transaction-process-session.ts
@@ -11,13 +11,45 @@ import {
 
 import { paymentAppFullyConfiguredEntrySchema } from "@/modules/payment-app-configuration/config-entry";
 import { getWebhookPaymentAppConfigurator } from "@/modules/payment-app-configuration/payment-app-configuration-factory";
-import { ChannelNotConfigured } from "@/errors";
+import { ChannelNotConfigured, UnsupportedEvent } from "@/errors";
 import { getHyperswitchAmountFromSaleorMoney, getSaleorAmountFromHyperswitchAmount } from "../hyperswitch/currencies";
 import { createHyperswitchClient } from "../hyperswitch/hyperswitch-api";
 import { type components as paymentsComponents } from "generated/hyperswitch-payments";
 import { validatePaymentCreateRequest } from "../hyperswitch/hyperswitch-api-request";
 import { intoPaymentResponse } from "../hyperswitch/hyperswitch-api-response";
 import { hyperswitchPaymentIntentToTransactionResult } from "./transaction-initialize-session";
+import { hyperswitchStatusToSaleorTransactionResult } from "@/pages/api/webhooks/hyperswitch/authorization";
+
+export const hyperswitchPaymentIntentToTransactionProcessResult = (
+  status: string,
+  transactionFlow: TransactionFlowStrategyEnum,
+): TransactionProcessSessionResponse["result"] => {
+  const prefix =
+  transactionFlow === TransactionFlowStrategyEnum.Authorization
+    ? "AUTHORIZATION"
+    : transactionFlow === TransactionFlowStrategyEnum.Charge
+      ? "CHARGE"
+      :
+        null;
+
+  invariant(prefix, `Unsupported transactionFlowStrategy: ${transactionFlow}`);
+
+  switch (status) {
+    case "succeeded":
+      return `${prefix}_SUCCESS`
+    case "failed":
+    case "cancelled":
+      return `${prefix}_FAILURE`
+    case "requires_capture":
+      return "AUTHORIZATION_SUCCESS"
+    case "requires_payment_method":
+    case "requires_customer_action":
+    case "requires_confirmation":
+      return `${prefix}_ACTION_REQUIRED`
+    default:
+      throw new UnsupportedEvent("This Event is not supported");
+  }
+};
 
 
 export const TransactionProcessSessionWebhookHandler = async (
@@ -51,96 +83,49 @@ export const TransactionProcessSessionWebhookHandler = async (
   const errors: SyncWebhookAppErrors = [];
   if (appChannelConfig == null) {
     throw new ChannelNotConfigured("Please assign a channel for your configuration");
-  }
-  let requestData = null;
-  if (event.data != null) {
-    requestData = validatePaymentCreateRequest(event.data);
   };
+
   const HyperswitchConfig = paymentAppFullyConfiguredEntrySchema.parse(appChannelConfig);
   const currency = event.action.currency;
-  const amount = getHyperswitchAmountFromSaleorMoney(event.action.amount, currency);
-  const billingAddress = event.sourceObject.billingAddress;
-  const shippingAddress = event.sourceObject.shippingAddress;
 
   const hyperswitchClient = createHyperswitchClient({
     apiKey: HyperswitchConfig.apiKey,
   });
-  const updateHyperswitchPayment = hyperswitchClient.path("/payments/{payment_id}").method("post").create();
+  const retrieveHyperswitchPayment = hyperswitchClient.path("/payments/{payment_id}").method("get").create();
+
+  const resource_id: paymentsComponents["schemas"]["PaymentIdType"] = {
+    PaymentIntentId: event.transaction.pspReference
+  };
 
   const capture_method =
     event.action.actionType == TransactionFlowStrategyEnum.Authorization ? "manual" : "automatic";
 
-  const updatePaymentPayload: paymentsComponents["schemas"]["PaymentsUpdateRequest"] = {
-    amount,
-    confirm: false,
-    currency: currency as paymentsComponents["schemas"]["PaymentsUpdateRequest"]["currency"],
-    capture_method,
-    customer_id: requestData?.customerId,
-    authentication_type: requestData?.authenticationType,
-    return_url: requestData?.returnUrl,
-    description: requestData?.description,
-    billing: {
-      address: {
-        line1: billingAddress?.streetAddress1,
-        line2: billingAddress?.streetAddress2,
-        city: billingAddress?.city,
-        state: billingAddress?.countryArea,
-        zip: billingAddress?.postalCode,
-        country: billingAddress?.country.code as paymentsComponents["schemas"]["CountryAlpha2"],
-        first_name: billingAddress?.firstName,
-        last_name: billingAddress?.lastName,
-      },
-      phone: {
-        number: billingAddress?.phone,
-      },
-      email: requestData?.billingEmail,
-    },
-    shipping: {
-      address: {
-        line1: shippingAddress?.streetAddress1,
-        line2: shippingAddress?.streetAddress2,
-        city: shippingAddress?.city,
-        state: shippingAddress?.countryArea,
-        zip: shippingAddress?.postalCode,
-        country: shippingAddress?.country.code as paymentsComponents["schemas"]["CountryAlpha2"],
-        first_name: shippingAddress?.firstName,
-        last_name: shippingAddress?.lastName,
-      },
-      phone: {
-        number: shippingAddress?.phone,
-      },
-      email: requestData?.shippingEmail,
-    },
-    metadata: {
-      channel_id: event.sourceObject.channel.id,
-      transaction_id: event.transaction.id,
-      saleor_api_url: saleorApiUrl,
-    },
+  const retrievePaymentPayload: paymentsComponents["schemas"]["PaymentsRetrieveRequest"] = {
+    force_sync: true,
+    resource_id,
   };
 
   const payment_id = event.transaction.pspReference;
 
-  const updatePaymentResponse = await updateHyperswitchPayment({
-    ...updatePaymentPayload,
+  const retrievePaymentResponse = await retrieveHyperswitchPayment({
+    ...retrievePaymentPayload,
     payment_id
   });
-  const updatePaymentResponseData = intoPaymentResponse(updatePaymentResponse.data);
+  const retrievePaymentResponseData = intoPaymentResponse(retrievePaymentResponse.data);
 
-  const result = hyperswitchPaymentIntentToTransactionResult(
-    updatePaymentResponseData.status,
+  const result = hyperswitchPaymentIntentToTransactionProcessResult(
+    retrievePaymentResponseData.status,
     event.action.actionType,
   );
   const transactionProcessSessionResponse: TransactionProcessSessionResponse = {
     data: {
-      clientSecret: updatePaymentResponseData.client_secret,
-      publishableKey: HyperswitchConfig.publishableKey,
       errors,
     },
-    pspReference: updatePaymentResponseData.payment_id,
+    pspReference: retrievePaymentResponseData.payment_id,
     result,
     amount: getSaleorAmountFromHyperswitchAmount(
-      updatePaymentResponseData.amount,
-      updatePaymentResponseData.currency,
+      retrievePaymentResponseData.amount,
+      retrievePaymentResponseData.currency,
     ),
     time: new Date().toISOString(),
   };
